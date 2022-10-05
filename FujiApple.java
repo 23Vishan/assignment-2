ed390bbc35c7faa7a8deef226c286efda2af9d3a
Vishan Patel <94568955+23Vishan@users.noreply.github.com>
2022-10-05 15:44:26 -0400

    Add files via upload

--- FujiApple.java
+++ FujiApple.java
@@ -9,25 +9,6 @@ public class FujiApple implements Product {
     public FujiApple() {
         this.name = "FujiApple";
         this.price = "$0.00";
-    }
-
-    @Override
-    public void setPrice(String filePath){
-            File file = new File(filePath);
-            Scanner scan = new Scanner(file);
-    
-            while(scan.hasNextLine())
-            {
-                String line = scan.nextLine();
-
-                String[] result = line.split(" ");
-
-                if (result[0].equals(name))
-                {
-                    price = result[1];
-                    break;
-                }
-            }
-            scan.close();
+        System.out.println("Created Fuji Apple");
     }
 }

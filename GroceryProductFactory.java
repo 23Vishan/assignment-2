00fd81358d63d479d1ebe2bd169c4bfdea6bdff8
Vishan Patel <94568955+23Vishan@users.noreply.github.com>
2022-10-26 11:23:27 -0400

    Delete GroceryProductFactory.java

--- GroceryProductFactory.java
+++
@@ -1,5 +0,0 @@
-// this is an interface, used to create factories for different product types
-public interface GroceryProductFactory {
-    void returnInventory();
-    Product makeProduct(String productName);
-}
